706dce62ff1c13ca30e54a83992369f169c945b4
Jabinesh <Jabislk7@gmail.com>
2022-04-21 16:16:43 +0200

    .

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class SecurityController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/security', name: 'app_security')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('security/index.html.twig', [
+            'controller_name' => 'SecurityController',
         ]);
     }
 }

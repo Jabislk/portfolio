fbfd7ac20e0714e70de2802b133e6019b9805947
Jabinesh <Jabislk7@gmail.com>
2022-04-25 16:43:30 +0200

    .

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\About;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AboutController extends AbstractController
 {
     #[Route('/about', name: 'app_about')]
     public function index(): Response
     {
+
+        $about = new About();
+        $about->setTitle('Jabinesh')
+            ->setDescription('lorem ipsum')
+            ->setImage('75');
+
         return $this->render('about/index.html.twig', [
             'controller_name' => 'AboutController',
+            "about" => $about
         ]);
     }
 }
